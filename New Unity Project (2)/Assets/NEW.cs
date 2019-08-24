e43b9695d7376d02d84e382dacafbc43ada522c8
seongji0125 <seongji0125@naver.com>
2019-08-24 14:11:07 +0900

    Update NEW.cs

--- New Unity Project (2)/Assets/NEW.cs
+++ New Unity Project (2)/Assets/NEW.cs
@@ -4,15 +4,4 @@ using UnityEngine;
 
 public class NEW : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
